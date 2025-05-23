befc0073d7b583be045c6e75e94e4207fe9fdc2b
esmeralda0147 <fajardo1400@gmail.com>
2025-05-22 20:37:24 -0500

    Fix styles and improve CSS organization

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import { CartProvider } from './context/CartContext';
 
