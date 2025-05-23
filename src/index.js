b648f16d8fa8631fcfef1cf8626ee3573bfe0615
esmeralda0147 <fajardo1400@gmail.com>
2025-05-22 20:29:01 -0500

    Update styles and Vercel configuration

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './styles.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
 
 import { CartProvider } from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // ⛔ Quitamos StrictMode solo para pruebas
     <CartProvider>
         <App />
     </CartProvider>
 );
-
